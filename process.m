411554eeec7e0860c30159b4a5502dfd28e46059
Qihao LIU <buaalqh@163.com>
2020-04-13 10:55:09 +0200

    update 13/04/2020

--- process.m
+++ process.m
@@ -8,9 +8,20 @@
 % timeIMU = datevec(Timestamps_IMU);
 % timeSLAM = datevec(Timestamps_SLAM);
 % loadSLAMCameraTrajectory;
-%  poseSLAM1;
+%%将SLAM旋转矩阵和平移向量写为4*4矩阵
+% poseSLAM=zeros(4,4,2762);
+% for i=1:2762
+%     for a=1:3
+%         for b=1:3
+%             poseSLAM(a,b,i)=SLAMCameraTrajectory(i,3*(a-1)+b);
+%             poseSLAM(4,4,i)=1;
+%             poseSLAM(a,4,i)=SLAMCameraTrajectory(i,a+9);
+%         end
+%     end
+% end
 % oxts = loadOxtsliteData('C:\Users\LQH\Documents\shareUbuntu\imu\drive_0018_sync');
 % poseIMU = convertOxtsToPose(oxts);
+
 %%%Euler angle synchronisation%%%
 %提取SLAM的欧拉角
 % R_SLAM_IMU=[0 -1 0;0 0 -1;1 0 0];
@@ -20,7 +31,23 @@
 % %      EulerSLAM(i,:)=[a,b,c]./pi.*180;
 %      EulerSLAM(i,:)=transpose(R_SLAM_IMU*transpose([a,b,c]))./pi.*180;
 % end
-%%提取从经纬度得来得欧拉角
+
+% %IMU的角速度数据提取 18，19，20
+% EulerIMU=zeros(2762,3);
+% R_SLAM_IMU=[0 0 1;-1 0 0;0 -1 0];
+% %angVinSLAM=zeros(2762,3);%%IMU到SLAM的坐标变化
+% for i=1:2762
+%     EulerIMU(i,1)=oxts{i}(18);
+%     EulerIMU(i,2)=oxts{i}(19);
+%     EulerIMU(i,3)=oxts{i}(20);
+%     EulerIMUinSLAM(i,:)=transpose((R_IMU_SLAM)*transpose(EulerIMU(i,:)));
+% end
+
+%%时间转换为min
+% t_IMU=timeIMU(:,5)*60+timeIMU(:,6)-54*60-39;
+% t_SLAM=timeSLAM(:,5)*60+timeSLAM(:,6)-54*60-39;
+
+%%提取从GPS经纬度得来得欧拉角
 % EulerIMU1=zeros(2762,3);
 % for i=1:2762
 %     [a,b,c]=dcm2angle(poseIMU{i}(1:3,1:3), 'ZYX');    
@@ -39,6 +66,8 @@
 %         EulerIMU2(i,1)=EulerIMU1(i,1);
 %     end
 % end
+
+%%********************************************%%
 %%转换IMU的角度范围从【-180，180】到【-90，90】
 %%EulerIMU是角速度,EulerIMU3是其积分
 % %%转换积分符号
@@ -57,6 +86,7 @@
 %         EulerIMU4(i,3)=EulerIMU(i,3)./pi.*180;
 %     end
 % end
+%%IMU数据需要第二次校对使其在【-90，90】范围内
 % for j=1:2762
 %     if EulerIMU3(j,1)>=90
 %         EulerIMU3(j,1)=180-EulerIMU3(j,1);
@@ -68,42 +98,24 @@
 %         EulerIMU3(j,1)=EulerIMU3(j,1);
 %     end
 % end
-% figure
-%%角速度
-% plot(t_IMU,-EulerIMU4(:,3),t_SLAM(2:2762),diff(EulerSLAM(:,1))./(t_SLAM(2:2762)-t_SLAM(1:2761))),title('yaw rate'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-% figure
-%角度
-% plot(t_IMU,EulerIMU3(:,1),t_SLAM,EulerSLAM(:,1)),title('yaw'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-
-
-% %IMU的角速度数据提取 18，19，20
-% EulerIMU=zeros(2762,3);
-% R_SLAM_IMU=[0 0 1;-1 0 0;0 -1 0];
-% %angVinSLAM=zeros(2762,3);%%IMU到SLAM的坐标变化
-% for i=1:2762
-%     EulerIMU(i,1)=oxts{i}(18);
-%     EulerIMU(i,2)=oxts{i}(19);
-%     EulerIMU(i,3)=oxts{i}(20);
-%     EulerIMUinSLAM(i,:)=transpose((R_IMU_SLAM)*transpose(EulerIMU(i,:)));
-% end
-
-%%时间转换为min
-% t_IMU=timeIMU(:,5)*60+timeIMU(:,6)-54*60-39;
-% t_SLAM=timeSLAM(:,5)*60+timeSLAM(:,6)-54*60-39;
-
-
+%%********************************************%%
+%%角速度（SLAM欧拉角转化为角速率）
+% figure,plot(t_IMU,-EulerIMU4(:,3),t_SLAM(2:2762),diff(EulerSLAM(:,1))./(t_SLAM(2:2762)-t_SLAM(1:2761))),title('yaw rate'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
+%%角度（IMU的角速率在之前已经积分）
+figure,plot(t_IMU,EulerIMU3(:,1),t_SLAM,EulerSLAM(:,1)),title('yaw'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
+%%********************************************%%
+% %对比GPS得来得欧拉角转换角度范围前后效果（用作坐标范围转化标准）
+% subplot(1,2,1),plot(t_IMU,EulerIMU1(:,1),t_SLAM,EulerSLAM(:,1)),title('yaw'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
+% subplot(1,2,2),plot(t_IMU,EulerIMU2(:,1),t_SLAM,EulerSLAM(:,1)),title('yaw'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
+%%****************
+%%SLAM和IMU欧拉角速率对比
 % figure
 % subplot(2,2,1),plot(t_IMU,-EulerIMU(:,1)./pi.*180,t_SLAM(2:2762),diff(EulerSLAM(:,3))./(t_SLAM(2:2762)-t_SLAM(1:2761))),title('roll rate'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
 % subplot(2,2,2),plot(t_IMU,-EulerIMU(:,2)./pi.*180,t_SLAM(2:2762),diff(EulerSLAM(:,2))./(t_SLAM(2:2762)-t_SLAM(1:2761))),title('pitch rate'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
 % subplot(2,2,3),plot(t_IMU,-EulerIMU4(:,3),t_SLAM(2:2762),diff(EulerSLAM(:,1))./(t_SLAM(2:2762)-t_SLAM(1:2761))),title('yaw rate'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-% figure
-% subplot(2,2,1),plot(t_IMU,EulerIMU1(:,3),t_SLAM,EulerSLAM(:,3)),title('roll'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-% subplot(2,2,2),plot(t_IMU,EulerIMU1(:,2),t_SLAM,EulerSLAM(:,2)),title('pitch'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-% %对比GPS得来得欧拉角转换角度范围前后效果
-% subplot(1,2,1),plot(t_IMU,EulerIMU1(:,1),t_SLAM,EulerSLAM(:,1)),title('yaw'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-% subplot(1,2,2),plot(t_IMU,EulerIMU2(:,1),t_SLAM,EulerSLAM(:,1)),title('yaw'),legend('IMU','SLAM'),ylabel('in degre'),xlabel('in seconde');
-
-run_demoVelodyne ('C:\Users\LQH\Documents\shareUbuntu\imu\drive_0018_sync','C:\Users\LQH\Documents\shareUbuntu\imu\drive_0018_sync\calib')
+%%**************
+%%提取ground-truth
+% run_demoVelodyne ('C:\Users\LQH\Documents\shareUbuntu\imu\drive_0018_sync','C:\Users\LQH\Documents\shareUbuntu\imu\drive_0018_sync\calib')
 
 
 
--- projection.m
+++ projection.m
@@ -2,10 +2,10 @@
 % lat:   latitude of the oxts-unit (deg)
 % lon:   longitude of the oxts-unit (deg)
 % alt:   altitude of the oxts-unit (m)
-mstruct=defaultm('mercator');
-mstruct.origin=[49.049519610770000,8.396596163994600,99.85]; %高度决定mercator的投影方向！！！
-mstruct.geoid=[6378137.0,0.01745329251994328];
-mstruct=defaultm(mstruct);
+% mstruct=defaultm('mercator');
+% mstruct.origin=[49.049519610770000,8.396596163994600,99.85]; %高度决定mercator的投影方向！！！
+% mstruct.geoid=[6378137.0,0.01745329251994328];
+% mstruct=defaultm(mstruct);
 %%地理坐标投影到直角坐标:
 % [x,y] =projfwd(mstruct,49.04951961077,8.3965961639946);
 % %直角坐标投影到地理坐标:该函数转换太慢
@@ -18,20 +18,21 @@ horizon=zeros(1,2762);
 % end
 % [x,y]=projfwd(mstruct,49.04951961077,8.3965961639946);
 
-%SLAM轨迹mercator方法投影到google earth上
-% poseSLAM=zeros(4,4,2762);
-% for i=1:2762
-%     for a=1:3
-%         for b=1:3
-%             poseSLAM(a,b,i)=SLAMCameraTrajectory(i,3*(a-1)+b);
-%             poseSLAM(4,4,i)=1;
-%             poseSLAM(a,4,i)=SLAMCameraTrajectory(i,a+9);
-%         end
-%     end
-% end
+%%SLAM轨迹mercator方法投影到google earth上
+%%poseSLAM见process.m
 % foward=poseSLAM(3,4,:);
 % horizon=poseSLAM(1,4,:);
 % % 问题发现：SLAM的数据只是前进方向的里程累积值，需要转换后再投影，（待日后解决）
+sens=0;
+for i=2:1000
+    j=i+48;
+    if j<=1000&&abs(EulerSLAM(j,1)-EulerSLAM(i,1))>30
+        sens=sens+EulerSLAM(j,1)-EulerSLAM(i,1);
+    end
+    foward(i)=foward(i-1)+(poseSLAM(3,4,i)-poseSLAM(3,4,i-1))*cos(sens);
+    horizon(i)=horizon(i-1)+(poseSLAM(3,4,i)-poseSLAM(3,4,i-1))*sin(sens);
+end
+figure,plot(horizon,foward);
 % ProjectionSLAM=zeros(2762,3);
 % for i=1:2762
 %     [ProjectionSLAM(i,2),ProjectionSLAM(i,1)]=minvtran(mstruct,foward(i),horizon(i));
@@ -57,26 +58,26 @@ horizon=zeros(1,2762);
 % % end
 
 %%IMU欧拉角mercator投影到google earth上
-for i=1:2762
-foward(i)=poseIMU{i}(1,4);
-horizon(i)=poseIMU{i}(2,4);
-end
-ProjectionIMU=zeros(2762,3);
-for i=1:2762
-    [ProjectionIMU(i,2),ProjectionIMU(i,1)] = minvtran(mstruct,foward(i),horizon(i));
-     ProjectionIMU(i,3)=0;
-end
+% for i=1:2762
+% foward(i)=poseIMU{i}(1,4);
+% horizon(i)=poseIMU{i}(2,4);
+% end
+% ProjectionIMU=zeros(2762,3);
+% for i=1:2762
+%     [ProjectionIMU(i,2),ProjectionIMU(i,1)] = minvtran(mstruct,foward(i),horizon(i));
+%      ProjectionIMU(i,3)=113;
+% end
 % dlmwrite('dataIMU.txt', ProjectionIMU);
 
 %%IMU数据按KML经纬度格式输出
-fid = fopen('C:\Users\LQH\Documents\shareUbuntu\imu\devkit\matlab\dataIMU.txt','w');
-for k = 1:2762
-for j = 1:2
-fprintf(fid,'%1.6f',ProjectionIMU(k,j));
-fprintf(fid,'%s',',');
-% fprintf(fid,'%s\t','一行finish');
-end
-fprintf(fid,'%1.6f\n',ProjectionIMU(k,3));
-% fprintf(fid,'%s\n',''------------------------------------------'');
-end 
-fclose(fid);
+% fid = fopen('C:\Users\LQH\Documents\shareUbuntu\imu\devkit\matlab\dataIMU.txt','w');
+% for k = 1:2762
+% for j = 1:2
+% fprintf(fid,'%1.6f',ProjectionIMU(k,j));
+% fprintf(fid,'%s',',');
+% % fprintf(fid,'%s\t','一行finish');
+% end
+% fprintf(fid,'%1.6f\n',ProjectionIMU(k,3));
+% % fprintf(fid,'%s\n',''------------------------------------------'');
+% end 
+% fclose(fid);
